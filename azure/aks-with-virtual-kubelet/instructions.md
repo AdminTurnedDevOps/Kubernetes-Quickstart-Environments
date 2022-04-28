f035e136b64e33bdcc285fbe6784ea041ff3a63b
Michael Levan <mlevan1992@gmail.com>
2022-04-28 10:25:41 -0400

    vnet and subnets for kubelet

--- azure/aks-with-virtual-kubelet/instructions.md
+++ azure/aks-with-virtual-kubelet/instructions.md
@@ -1,5 +1,16 @@
 After you deploy AKS, you'll need to add the virtual-kubelet add-on. To do that:
 
+1. Run the following command to connect to the AKS cluster from a terminal:
 ```
-az aks enable-addons --name aksenvironment-vkubelet-01 --resource-group resource_group_name --addons virtual-node --subnet-name aks_vet_subnet_name
+az aks get-credentials --name aksenvironment-vkubelet-01 --resource-group resource_group_name
+```
+
+1. Add a new subnet to the AKS vNet. If you don't, you'll see an error similar to the below:
+```
+got HTTP response status code 400 error code \"SubnetDelegationsCannotChangeWhenSubnetUsedByResource\": Delegations of subnet cannot be changed from [] to [Microsoft.ContainerInstance/containerGroups] because it is being used by the resource
+```
+
+2. Run the `enable-addons` command:
+```
+az aks enable-addons --name aksenvironment-vkubelet-01 --resource-group resource_group_name --addons virtual-node --subnet-name akssubnetvkube
 ```
--- azure/aks-with-virtual-kubelet/main.tf
+++ azure/aks-with-virtual-kubelet/main.tf
@@ -11,6 +11,27 @@ provider "azurerm" {
   features {}
 }
 
+resource "azurerm_virtual_network" "aksnet" {
+  name                = "aksnet"
+  address_space       = ["192.168.0.0/16"]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_subnet" "akssubnet" {
+  name                 = "akssubnet01"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.aksnet.name
+  address_prefixes     = ["192.168.1.0/24"]
+}
+
+resource "azurerm_subnet" "akssubnet-vkube" {
+  name                 = "akssubnetvkube"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.aksnet.name
+  address_prefixes     = ["192.168.100.0/24"]
+}
+
 resource "azurerm_kubernetes_cluster" "k8squickstart" {
   name                = var.name
   location            = var.location
@@ -25,6 +46,7 @@ resource "azurerm_kubernetes_cluster" "k8squickstart" {
     name       = "default"
     node_count = var.node_count
     vm_size    = "Standard_A2_v2"
+    vnet_subnet_id = azurerm_subnet.akssubnet.id
   }
 
   identity {
